d1262a39b1b2976a7f62bed06b212d94f90db6fd
d3e2j2m1p1-wq <d3e2j2m1p1@gmail.com>
2026-03-12 18:23:34 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'classmap-v1';
+const ASSETS = [
+  '/ClassMap/',
+  '/ClassMap/index.html',
+  '/ClassMap/manifest.json',
+  '/ClassMap/icon-192.png',
+  '/ClassMap/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached =>
+      cached || fetch(e.request).then(res => {
+        if (res && res.status === 200 && e.request.method === 'GET') {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      })
+    )
+  );
+});
